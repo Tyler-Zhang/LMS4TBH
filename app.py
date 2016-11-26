69f7fdd3df690bc376a3f03d17362c10b521b6d3
Kevin Pei <kevin-pei@hotmail.com>
2016-11-26 14:49:12 -0500

    test

--- app.py
+++ app.py
@@ -1,11 +1,62 @@
 import os
-from flask import Flask, request, send_from_directory
+from datetime import timedelta
+from flask import Flask, send_from_directory, make_response, request, current_app
+from functools import update_wrapper
+from app import responder
+
 app = Flask(__name__, static_url_path='')
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 @app.route('/public/<path:path>')
 def send_js(path):
     return send_from_directory('public', path)
 
+@app.route('/respond', methods=['POST'])
+@crossdomain(origin='*')
+def respond():
+    text = request.form['text']
+    return responder.respond(text)
 
 
 if __name__ == '__main__':
