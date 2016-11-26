bba797fb5470bfa7ac17ff40a1a52473d62793d9
Kevin Pei <kevin-pei@hotmail.com>
2016-11-26 14:26:40 -0500

    static

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, send_from_directory
+app = Flask(__name__, static_url_path='')
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/public/<path:path>')
+def send_js(path):
+    return send_from_directory('public', path)
+
 
 
 if __name__ == '__main__':
