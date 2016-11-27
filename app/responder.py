ee61592933aa2e527e0edb70a31716e492b74a1e
Kevin Pei <kevin-pei@hotmail.com>
2016-11-26 20:10:36 -0500

    lol wut

--- app/responder.py
+++ app/responder.py
@@ -2,11 +2,13 @@ import markovify
 
 def respond(text):
     text_model = markovify.Text(text.lower())
+    print(text.lower())
     msg = None
     try:
         msg = text_model.make_sentence_with_start("you")
     except: 
         False
+    print(msg)
     if (msg == None):
         msg = text_model.make_sentence()
     if (msg != None):
